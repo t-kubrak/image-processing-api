6fa941f26f964f5019bd28f759ece6c1ec9870b5
t-kubrak <chikashikaari@gmail.com>
2022-08-28 10:51:53 +0300

    fix: create thumb folder if doesn't exist, add endpoint parameter and return types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { process, exists } from './image-processor';
 import path from 'path';
+import { mkdir } from 'fs/promises';
 
 const app = express();
 const port = 3000;
@@ -10,31 +11,39 @@ app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
 
-app.get('/api/images', async (req, res) => {
-    const filename = req.query.filename as unknown as string;
-    const width = req.query.width as unknown as string;
-    const height = req.query.height as unknown as string;
-    const imageInput = path.resolve(`images/full/${filename}.jpg`);
-    const imageOutput = path.resolve(
-        `images/thumb/${filename}-${width}-${height}.jpg`
-    );
+app.get(
+    '/api/images',
+    async (req: Request, res: Response): Promise<Response | void> => {
+        const filename = req.query.filename as unknown as string;
+        const width = req.query.width as unknown as string;
+        const height = req.query.height as unknown as string;
+        const imageInput = path.resolve(`images/full/${filename}.jpg`);
+        const imageOutput = path.resolve(
+            `images/thumb/${filename}-${width}-${height}.jpg`
+        );
+        const thumbFolder = path.resolve('images/thumb');
 
-    if (!width || !height) {
-        return res.sendFile(imageInput);
+        if (!width || !height) {
+            return res.sendFile(imageInput);
+        }
+
+        if (!(await exists(thumbFolder))) {
+            await mkdir(thumbFolder);
+        }
+
+        if (await exists(imageOutput)) {
+            return res.sendFile(imageOutput);
+        }
+
+        try {
+            await process(imageInput, imageOutput, width, height);
+
+            return res.sendFile(imageOutput);
+        } catch (e) {
+            console.error(e);
+            res.status(500).send(`Couldn't serve the image specified`);
+        }
     }
-
-    if (await exists(imageOutput)) {
-        return res.sendFile(imageOutput);
-    }
-
-    try {
-        await process(imageInput, imageOutput, width, height);
-
-        return res.sendFile(imageOutput);
-    } catch (e) {
-        console.error(e);
-        res.status(500).send(`Couldn't serve the image specified`);
-    }
-});
+);
 
 export default app;
